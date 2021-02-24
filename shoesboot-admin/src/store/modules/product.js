7cbd54271e529cc33d51831c3932e9e1ac86b6eb
Shenhongtao12 <shenhongtao12@aliyun.com>
2021-02-24 21:45:18 +0800

    admin model init

---
+++ shoesboot-admin/src/store/modules/product.js
@@ -0,0 +1,15 @@
+const product = {
+  state: {
+    pro: {},
+  },
+
+  mutations: {
+    SET_PRODUCT: (state, pro) => {
+      state.pro = pro;
+    },
+  },
+
+  actions: {},
+};
+
+export default product;
