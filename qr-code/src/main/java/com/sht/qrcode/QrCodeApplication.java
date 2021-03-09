5483026237b92c782f14812a9dc73a9890ab1b47
Shenhongtao12 <shenhongtao12@aliyun.com>
2021-03-09 21:24:33 +0800

    config

--- qr-code/src/main/java/com/sht/qrcode/QrCodeApplication.java
+++ qr-code/src/main/java/com/sht/qrcode/QrCodeApplication.java
@@ -2,8 +2,10 @@ package com.sht.qrcode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan("com.sht.qrcode.mapper")
 public class QrCodeApplication {
 
     public static void main(String[] args) {
